03b6daa576e37cac6834bcd0922ee7222b0d6c11
Matthew Harrilal <matthewharrilal@gmail.com>
2019-02-14 12:25:28 -0800

    Added first configuration function to main

--- main.go
+++ main.go
@@ -2,8 +2,21 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"os"
+	"github.com/joho/godotenv"
 )
 
-func AccountConfiguration() {
-	accountSID := 
+func AccountConfiguration() (string, string, string) {
+	err := godotenv.Load() // First load environment variables file
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	accountSID := os.Getenv("ACCOUNT_SID")
+	authToken := os.Getenv("AUTH_TOKEN")
+
+	url := fmt.Sprintf("https://api.twilio.com/2010-04-01/Accounts/%v/Messages.json", accountSID)
+
+	return accountSID, authToken, url
 }
