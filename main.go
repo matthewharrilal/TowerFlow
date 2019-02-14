e28e7e4b72c5f54d5585e1dc1dd00496ef54047b
Matthew Harrilal <matthewharrilal@gmail.com>
2019-02-14 12:45:43 -0800

    Completed the configuration of the message

--- main.go
+++ main.go
@@ -4,10 +4,15 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"strings"
+	"net/url"
+	"net/http"
 	"github.com/joho/godotenv"
 )
 
 func AccountConfiguration() (string, string, string) {
+	// Loads our environement variables and configures url that we are going to be pinging
+
 	err := godotenv.Load() // First load environment variables file
 	if err != nil {
 		log.Fatal(err)
@@ -16,7 +21,30 @@ func AccountConfiguration() (string, string, string) {
 	accountSID := os.Getenv("ACCOUNT_SID")
 	authToken := os.Getenv("AUTH_TOKEN")
 
-	url := fmt.Sprintf("https://api.twilio.com/2010-04-01/Accounts/%v/Messages.json", accountSID)
+	url := fmt.Printf("https://api.twilio.com/2010-04-01/Accounts/%v/Messages.json", accountSID)
 
 	return accountSID, authToken, url
 }
+
+func ConstructMessage() strings.Reader {
+	// Constructs message object with given source and destination
+	// _, _, url := AccountConfiguration()
+
+	messageData := url.Values{} // Used to store and encode following parameters to be sent over the network
+	destinationNumber, sourceNumber := "7183009363", "6468324582"
+	messageStub := "You are receiving a test message"
+	
+	// Setting source number and destination number
+	messageData.Set("From", sourceNumber)
+	messageData.Set("To", destinationNumber)
+
+	
+	messageData.Set("Body", messageStub)
+
+	messageDataReader := *strings.NewReader(messageData.Encode())
+	fmt.Printf("Message Data Reader ", messageDataReader)
+	return messageDataReader
+}
+
+
+
