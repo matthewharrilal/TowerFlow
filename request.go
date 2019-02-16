3e0f64208edaaf554a8d8b1b417733d941192903
Matthew Harrilal <matthewharrilal@gmail.com>
2019-02-16 11:50:17 -0800

    In the process of completing the request executor function

--- request.go
+++ request.go
@@ -7,24 +7,42 @@ import (
 	"strings"
 )
 
-// func ConstructRequest(destinationNumber string) (http.Client, *http.Request) {
-// 	accountSID, authToken, urlString := AccountConfiguration()
-// 	messageDataReader := ConstructMessage(destinationNumber)
+// // What pairing does this function return
+// func ExecuteRequest(destinationNumber string, channel chan Message) (Message, error) {
+// 	// Access to the request executor and the request itself with configurations already implemented
+// 	client, request := ConstructRequest(destinationNumber)
+// 	var message Message
 
-// 	client := http.Client{} // In charge of executing the request
+// 	dataCopy := &Message{}
+// 	response, err := client.Do(request) // Execute the request and store the response
 
-// 	// Formulate POST request with the given url string, and the encoded representation of the message body
-// 	request, _ := http.NewRequest("POST", urlString, &messageDataReader) // Passing the message data reader by reference
+// 	// If there was an error executing the request
+// 	if err != nil {
+// 		fmt.Println("Error executing the request")
+// 		log.Fatal(err)
+// 	}
 
-// 	// Adds header field with the key name 'Authorization' and the two credentials we send as values to the Twillio API
-// 	request.SetBasicAuth(accountSID, authToken)
+// 	// Checking if response came back successful
+// 	if response.StatusCode >= 200 && response.StatusCode < 300 {
+// 		// Data consisting of string keys and dynamic value types depending on the JSON coming back
 
-// 	// Additional header fields to accept json media types which can be used for the response
-// 	request.Header.Add("Accept", "application/json")
+// 		// Decode the response body
+// 		decoder := json.NewDecoder(response.Body)
 
-// 	// To indicate the media type that is being sent through the request
-// 	request.Header.Add("Content-Type", "application/x-www-form-urlencoded")
-// 	return client, request
+// 		err := decoder.Decode(&message) // Read the decoded data into our data map
+
+// 		if err != nil {
+// 			log.Fatal(err)
+// 			return Message{}, err
+// 		}
+// 		// Pass in a channel and pass the message that was created through the channel and recieve it
+// 		dataCopy = &message
+// 	} else {
+// 		fmt.Printf("Status Code not successful ", response.StatusCode)
+// 	}
+
+// 	channel <- *dataCopy
+// 	return *dataCopy, nil
 // }
 
 func (client *Client) NewRequest(httpMethod string, messageDataBuffer *strings.Reader) *http.Request {
@@ -41,11 +59,25 @@ func (client *Client) NewRequest(httpMethod string, messageDataBuffer *strings.R
 	// Should the header fields be static ... depending on what client is using this service it is going to have to be dynamic
 	// We will make that dynamic in the next step
 
-
 	// Additional header fields to accept json media types which can be used for the response
 	request.Header.Add("Accept", "application/json")
 
 	// To indicate the media type that is being sent through the request
 	request.Header.Add("Content-Type", "application/x-www-form-urlencoded")
 	return request
+}
+
+func (client *Client) ExecuteRequest(httpMethod string, destinationNumber string, messageChannel chan Message) {
+	var messageCopy Message
+
+	messageDataBuffer := client.NewMessage(destinationNumber)
+
+	request := client.NewRequest(httpMethod, messageDataBuffer)
+
+	response, err := client.RequestExecutor.Do(request)
+
+	if err != nil {
+		fmt.Println("ERROR EXECUTING THE REQUEST")
+		log.Fatal(err)
+	}
 }
