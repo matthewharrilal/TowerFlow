3e4463d995ea255178154cbf63c4b7d10d91a4a8
Matthew Harrilal <matthewharrilal@gmail.com>
2019-02-15 16:05:27 -0800

    Able to call account configuration function from different file

--- main.go
+++ main.go
@@ -6,32 +6,11 @@ import (
 	"log"
 	"net/http"
 	"net/url"
-	"os"
 	"strings"
-
-	"github.com/joho/godotenv"
 )
-
-func AccountConfiguration() (string, string, string) {
-	// Loads our environement variables and configures url that we are going to be pinging
-
-	err := godotenv.Load() // First load environment variables file
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	accountSID := os.Getenv("ACCOUNT_SID")
-	authToken := os.Getenv("AUTH_TOKEN")
-
-	url := fmt.Sprintf("https://api.twilio.com/2010-04-01/Accounts/%v/Messages.json", accountSID)
-
-	return accountSID, authToken, url
-}
 
 func ConstructMessage(destinationNumber string) strings.Reader {
 	// Constructs message object with given source and destination
-	// _, _, url := AccountConfiguration()
-
 	messageData := url.Values{} // Used to store and encode following parameters to be sent over the network
 	sourceNumber := "6468324582"
 	messageStub := "Hey, this is Matthew and this is my project for school. It allows me to be able to send text messages to a group of people simultaneously"
@@ -105,7 +84,7 @@ func ExecuteRequest(destinationNumber string, channel chan map[string]interface{
 }
 
 func main() {
-	destinationNumbers := []string{"7183009363", "7025305234"}
+	destinationNumbers := []string{"7183009363"}
 	channel := make(chan map[string]interface{})
 	for _, number := range destinationNumbers {
 		go ExecuteRequest(number, channel)
