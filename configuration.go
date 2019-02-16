a1f9e6963cbfed793b939ea9d24795bc69cfa690
Matthew Harrilal <matthewharrilal@gmail.com>
2019-02-15 16:04:55 -0800

    Seperated configuration logic to different file

---
+++ configuration.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+)
+
+func AccountConfiguration() (string, string, string) {
+	// Loads our environement variables and configures url that we are going to be pinging
+
+	err := godotenv.Load() // First load environment variables file
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	accountSID := os.Getenv("ACCOUNT_SID")
+	authToken := os.Getenv("AUTH_TOKEN")
+
+	url := fmt.Sprintf("https://api.twilio.com/2010-04-01/Accounts/%v/Messages.json", accountSID)
+
+	return accountSID, authToken, url
+}
