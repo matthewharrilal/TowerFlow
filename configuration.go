0d3c18d10cb5c0056842258079972cbccec21b80
Matthew Harrilal <matthewharrilal@gmail.com>
2019-02-16 09:40:49 -0800

    Refacotored client struct and account configuration method

--- configuration.go
+++ configuration.go
@@ -4,17 +4,31 @@ import (
 	"fmt"
 	"log"
 	"os"
-
+	"net/http"
 	"github.com/joho/godotenv"
 )
 
 type Client struct {
 	// What is the neccesary information needed to create a client?
+	RequestExecutor http.Client // Each individual has the ability to execute their request with the added configurations
+
 	AuthToken string 
 
 	AccountSID string 
 
 	Url string 
+}
+
+// Constructs a new client with the given credentials
+
+func NewClient(requestExecutor http.Client, authToken string, accountSID string) Client {
+	baseURL := fmt.Sprintf("https://api.twilio.com/2010-04-01/Accounts/%v/Messages.json", accountSID)
+
+	client := Client{requestExecutor, authToken, accountSID, baseURL} // Creating a client with the configurations added 
+
+	// Now that we have the client with the configurations added 
+
+	return client
 }
 
 func AccountConfiguration() (string, string, string) {
