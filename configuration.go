995110245191f6a663157df687f989e9da2dc464
Matthew Harrilal <matthewharrilal@gmail.com>
2019-02-16 10:04:43 -0800

    Added attribute to client struct called  source number

--- configuration.go
+++ configuration.go
@@ -2,47 +2,48 @@ package main
 
 import (
 	"fmt"
-	"log"
-	"os"
 	"net/http"
-	"github.com/joho/godotenv"
 )
 
 type Client struct {
 	// What is the neccesary information needed to create a client?
 	RequestExecutor http.Client // Each individual has the ability to execute their request with the added configurations
 
-	AuthToken string 
+	SourceNumber string
 
-	AccountSID string 
+	AuthToken string
 
-	Url string 
+	AccountSID string
+
+	BaseURL string
 }
 
 // Constructs a new client with the given credentials
 
-func NewClient(requestExecutor http.Client, authToken string, accountSID string) Client {
+// func AccountConfiguration() (string, string, string) {
+// 	// Loads our environement variables and configures url that we are going to be pinging
+
+// 	err := godotenv.Load() // First load environment variables file
+// 	if err != nil {
+// 		log.Fatal(err)
+// 	}
+
+// 	accountSID := os.Getenv("ACCOUNT_SID")
+// 	authToken := os.Getenv("AUTH_TOKEN")
+
+// 	url := fmt.Sprintf("https://api.twilio.com/2010-04-01/Accounts/%v/Messages.json", accountSID)
+
+// 	return accountSID, authToken, url
+// }
+
+func NewClient(requestExecutor http.Client, sourceNumber string, authToken string, accountSID string) Client {
+	// In charge of creating a client capable of executing requests with dynamic configurations already attached
+
 	baseURL := fmt.Sprintf("https://api.twilio.com/2010-04-01/Accounts/%v/Messages.json", accountSID)
 
-	client := Client{requestExecutor, authToken, accountSID, baseURL} // Creating a client with the configurations added 
+	client := Client{requestExecutor, sourceNumber, authToken, accountSID, baseURL} // Creating a client with the configurations added
 
-	// Now that we have the client with the configurations added 
+	// Now that we have the client with the configurations added
 
 	return client
-}
-
-func AccountConfiguration() (string, string, string) {
-	// Loads our environement variables and configures url that we are going to be pinging
-
-	err := godotenv.Load() // First load environment variables file
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	accountSID := os.Getenv("ACCOUNT_SID")
-	authToken := os.Getenv("AUTH_TOKEN")
-
-	url := fmt.Sprintf("https://api.twilio.com/2010-04-01/Accounts/%v/Messages.json", accountSID)
-
-	return accountSID, authToken, url
 }
