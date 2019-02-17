5cb4585a108a385c06996391361c5856eea1b298
Matthew Harrilal <matthewharrilal@gmail.com>
2019-02-16 17:13:43 -0800

    Added corresponding test file

--- request.go
+++ request.go
@@ -8,46 +8,7 @@ import (
 	"strings"
 )
 
-// // What pairing does this function return
-// func ExecuteRequest(destinationNumber string, channel chan Message) (Message, error) {
-// 	// Access to the request executor and the request itself with configurations already implemented
-// 	client, request := ConstructRequest(destinationNumber)
-// 	var message Message
-
-// 	dataCopy := &Message{}
-// 	response, err := client.Do(request) // Execute the request and store the response
-
-// 	// If there was an error executing the request
-// 	if err != nil {
-// 		fmt.Println("Error executing the request")
-// 		log.Fatal(err)
-// 	}
-
-// 	// Checking if response came back successful
-// 	if response.StatusCode >= 200 && response.StatusCode < 300 {
-// 		// Data consisting of string keys and dynamic value types depending on the JSON coming back
-
-// 		// Decode the response body
-// 		decoder := json.NewDecoder(response.Body)
-
-// 		err := decoder.Decode(&message) // Read the decoded data into our data map
-
-// 		if err != nil {
-// 			log.Fatal(err)
-// 			return Message{}, err
-// 		}
-// 		// Pass in a channel and pass the message that was created through the channel and recieve it
-// 		dataCopy = &message
-// 	} else {
-// 		fmt.Printf("Status Code not successful ", response.StatusCode)
-// 	}
-
-// 	channel <- *dataCopy
-// 	return *dataCopy, nil
-// }
-
 func (client *Client) NewRequest(httpMethod string, messageDataBuffer *strings.Reader) *http.Request {
-	// have to verify that the http method that the user passes in is valid
 	request, err := http.NewRequest(httpMethod, client.BaseURL, messageDataBuffer)
 
 	if err != nil {
@@ -69,17 +30,14 @@ func (client *Client) NewRequest(httpMethod string, messageDataBuffer *strings.R
 	return request
 }
 
-func (client *Client) ExecuteRequest(httpMethod string, destinationNumber string, messageContent string,  messageChannel chan Message) Message {
+func (client *Client) ExecuteRequest(httpMethod string, destinationNumber string, messageContent string, messageChannel chan Message) Message {
 	// Returns you a message Object back
 
-	fmt.Println("ENTERING")
 	var message Message
 
 	messageDataBuffer := client.NewMessage(messageContent, destinationNumber)
 
-	// fmt.Printf("Message Data Bufffer ", messageDataBuffer)
 	request := client.NewRequest(httpMethod, messageDataBuffer)
-	// fmt.Printf("REQUEST ", request)
 
 	response, err := client.RequestExecutor.Do(request)
 
@@ -88,7 +46,6 @@ func (client *Client) ExecuteRequest(httpMethod string, destinationNumber string
 		log.Fatal(err)
 	}
 
-	// fmt.Printf("RESPONSE ", response)
 	if response.StatusCode >= 300 {
 		err := fmt.Sprintf("Status Code :", response.StatusCode)
 		log.Fatal(err)
@@ -100,8 +57,6 @@ func (client *Client) ExecuteRequest(httpMethod string, destinationNumber string
 	if err != nil {
 		log.Fatal(err)
 	}
-
-	fmt.Printf("Message -> ", message)
 	messageChannel <- message
 	return message
 }
